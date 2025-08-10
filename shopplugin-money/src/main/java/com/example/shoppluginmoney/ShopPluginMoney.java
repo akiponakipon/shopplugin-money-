bb7e6ee5f4cff03a88738e72b23e6da15455d79a
akiponakipon <ojiakaoldriver@gmail.com>
2025-08-10 15:36:27 +0900

    Update ShopPluginMoney.java

--- shopplugin-money/src/main/java/com/example/shoppluginmoney/ShopPluginMoney.java
+++ shopplugin-money/src/main/java/com/example/shoppluginmoney/ShopPluginMoney.java
@@ -1,28 +1,18 @@
 package com.example.shoppluginmoney;
 
+import com.example.shoppluginmoney.commands.SellCommand;
+import com.example.shoppluginmoney.commands.SellOffCommand;
+import com.example.shoppluginmoney.commands.ShopMoneyCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ShopPluginMoney extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        getCommand("sell").setExecutor(new SellCommand());
+        getCommand("selloff").setExecutor(new SellOffCommand());
+        getCommand("shopmoney").setExecutor(new ShopMoneyCommand());
         getLogger().info("ShopPluginMoney が有効になりました！");
-
-        // コマンド登録
-        getCommand("sell").setExecutor((sender, command, label, args) -> {
-            sender.sendMessage("§a[Shop] §f売却コマンドが実行されました！");
-            return true;
-        });
-
-        getCommand("selloff").setExecutor((sender, command, label, args) -> {
-            sender.sendMessage("§c[Shop] §f売却停止コマンドが実行されました！");
-            return true;
-        });
-
-        getCommand("shopmoney").setExecutor((sender, command, label, args) -> {
-            sender.sendMessage("§e[Shop] §f現在の所持金を確認します！");
-            return true;
-        });
     }
 
     @Override
@@ -30,3 +20,4 @@ public class ShopPluginMoney extends JavaPlugin {
         getLogger().info("ShopPluginMoney が無効になりました！");
     }
 }
+
